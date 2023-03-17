c29f47a3edc6cfa3e999c980009d97f432c45d8e
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2023-03-17 12:42:01 +0100

    remove warning due to md5

--- ir-datasets/tutorial/pangrams.py
+++ ir-datasets/tutorial/pangrams.py
@@ -16,7 +16,7 @@ class PangramDocument(NamedTuple):
 
 ir_datasets.registry.register('pangrams', Dataset(
     JsonlDocs(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'pangram-documents.jsonl')], expected_md5='3f67adc5d99a7b6b7a410d4aefc8fe3b'), doc_cls=PangramDocument, lang='en'),
-    TrecXmlQueries(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'pangram-topics.xml')]), lang='en'),
+    TrecXmlQueries(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'pangram-topics.xml')], expected_md5='411647769eabf8dbcaac85cdb734c50d'), lang='en'),
     TrecQrels(ir_datasets.util.Download([RequestsDownload(DATASET_URL + 'pangram-qrels.txt')], expected_md5='2ef82edf2e8c1f6724e92d9f422b3f5f'), {0: 'Not Relevant', 1: 'Relevant'})
 ))
 
