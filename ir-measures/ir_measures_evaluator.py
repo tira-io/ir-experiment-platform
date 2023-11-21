8a08884d96a80359b2c6f700f1ea27d945b90bd5
Maik Fröbe <maik.froebe@uni-jena.de>
2023-11-21 15:09:38 +0100

    Simplify ir-measures evaluator

--- ir-measures/ir_measures_evaluator.py
+++ ir-measures/ir_measures_evaluator.py
@@ -6,6 +6,7 @@ import os
 import re
 from pathlib import Path
 from typing import Tuple, Dict, List, Optional
+import gzip
 
 import ir_measures
 from ir_measures import Qrel, ScoredDoc, Measure, Metric
@@ -876,14 +877,8 @@ def render_results(run_file, irds_id, output_path, top_k=10):
     excerpt_for_rendering['queries'] = {k: json.loads(irds_loader.map_query_as_jsonl(v)) for k,v in excerpt_for_rendering['queries'].items()}
     excerpt_for_rendering['documents'] = {k: json.loads(irds_loader.map_doc(v)) for k,v in excerpt_for_rendering['documents'].items()}
 
-    with open(Path(output_path) / '.data-top-10-for-rendering.jsonl', 'w') as output_file:
+    with gzip.open(Path(output_path) / '.data-top-10-for-rendering.json.gz', 'w') as output_file:
         output_file.write(json.dumps(excerpt_for_rendering))
-
-    from diffir.run import diff_from_local_data
-    _, rendered_serp = diff_from_local_data([str(Path(run_file).resolve())], [str((Path(output_path) / '.data-top-10-for-rendering.jsonl').resolve())], cli=False, web=True, print_html=False)
-
-    with open(Path(output_path) / 'serp.html', 'w') as output_file:
-        output_file.write(rendered_serp)
 
 
 def irds_id_from_metadata_or_none(f):
